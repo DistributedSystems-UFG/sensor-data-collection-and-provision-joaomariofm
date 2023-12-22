3acf1383079add7c469de28eeaa3ce133e9665a1
João Mário <joaomf17p2@gmail.com>
2023-12-22 19:46:36 -0300

    sending sensor data to mqtt

--- app/src/main/java/com/example/samplesensorproviderapp/LightSensorAccess.java
+++ app/src/main/java/com/example/samplesensorproviderapp/LightSensorAccess.java
@@ -6,10 +6,18 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.widget.TextView;
 
+import com.hivemq.client.mqtt.datatypes.MqttQos;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
+
+import java.util.UUID;
+
 public class LightSensorAccess implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor mLight;
     private TextView sensor_field;
+
+    public static final String brokerURI = "3.89.70.15";
 
     public LightSensorAccess(SensorManager sm, TextView tv){
         sensorManager = sm;
@@ -17,6 +25,7 @@ public class LightSensorAccess implements SensorEventListener {
         mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
     @Override
     public final void onAccuracyChanged(Sensor sensor, int accuracy) {
         // Do something here if sensor accuracy changes.
@@ -28,6 +37,23 @@ public class LightSensorAccess implements SensorEventListener {
         float lux = event.values[0];
         // Show luminosity value on the text field
         sensor_field.setText(String.valueOf(lux));
+
+        this.sendTemperatureValue(String.valueOf(lux));
+    }
+
+    public final void sendTemperatureValue(String value) {
+        Mqtt5BlockingClient client = Mqtt5Client.builder()
+                .identifier(UUID.randomUUID().toString())
+                .serverHost(brokerURI)
+                .buildBlocking();
+
+        client.connect();
+        client.publishWith()
+                .topic("/luminosity-value")
+                .qos(MqttQos.AT_LEAST_ONCE)
+                .payload(String.valueOf(value).getBytes())
+                .send();
+        client.disconnect();
     }
 
     @Override
--- app/src/main/java/com/example/samplesensorproviderapp/TemperatureSensorAccess.java
+++ app/src/main/java/com/example/samplesensorproviderapp/TemperatureSensorAccess.java
@@ -6,10 +6,18 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.widget.TextView;
 
+import com.hivemq.client.mqtt.datatypes.MqttQos;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
+
+import java.util.UUID;
+
 public class TemperatureSensorAccess implements SensorEventListener  {
     private SensorManager sensorManager;
     private Sensor mAmbientTemperature;
     private TextView sensor_field;
+
+    public static final String brokerURI = "3.89.70.15";
 
     public TemperatureSensorAccess(SensorManager sm, TextView tv){
         sensorManager = sm;
@@ -24,10 +32,27 @@ public class TemperatureSensorAccess implements SensorEventListener  {
 
     @Override
     public final void onSensorChanged(SensorEvent event) {
-        // The light sensor returns a single value.
-        float lux = event.values[0];
+        // The temperature sensor returns a single value.
+        float temp = event.values[0];
         // Show luminosity value on the text field
-        sensor_field.setText(String.valueOf(lux));
+        sensor_field.setText(String.valueOf(temp));
+
+        this.sendTemperatureValue(String.valueOf(temp));
+    }
+
+    public final void sendTemperatureValue(String value) {
+        Mqtt5BlockingClient client = Mqtt5Client.builder()
+                .identifier(UUID.randomUUID().toString())
+                .serverHost(brokerURI)
+                .buildBlocking();
+
+        client.connect();
+        client.publishWith()
+                .topic("/temperature-value")
+                .qos(MqttQos.AT_LEAST_ONCE)
+                .payload(String.valueOf(value).getBytes())
+                .send();
+        client.disconnect();
     }
 
     @Override
